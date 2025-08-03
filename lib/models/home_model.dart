503d81c79b6f6c825b9b5e3997c71c0e1f86357d
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-08-03 17:26:08 +0300

    add required modelsl in home tab

--- lib/models/home_model.dart
+++ lib/models/home_model.dart
@@ -1,1 +1,54 @@
-class HomeModel {}
+class Car {
+  final String title;
+  final String year;
+  final String category;
+  final int price;
+  final int km;
+  final String transmission;
+  final String monthlyPayment;
+  final bool isNew;
+
+  Car({
+    required this.title,
+    required this.year,
+    required this.category,
+    required this.price,
+    required this.km,
+    required this.transmission,
+    required this.monthlyPayment,
+    required this.isNew,
+  });
+}
+
+class Brand {
+  final String name;
+  final String logoUrl;
+
+  Brand({required this.name, required this.logoUrl});
+}
+
+class CertifiedBenefit {
+  final String title;
+  final String description;
+  final String iconPath; // Can be an asset or network icon
+
+  CertifiedBenefit({
+    required this.title,
+    required this.description,
+    required this.iconPath,
+  });
+}
+
+class PurchaseStep {
+  final int stepNumber;
+  final String title;
+  final String description;
+  final String iconPath;
+
+  PurchaseStep({
+    required this.stepNumber,
+    required this.title,
+    required this.description,
+    required this.iconPath,
+  });
+}
